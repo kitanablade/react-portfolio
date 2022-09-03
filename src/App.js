614e40b76a6544312a26d559c89eac41bf669786
kitanablade <kiteagle@gmail.com>
2022-09-03 13:12:46 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer/>
       <h1 className="text-danger">Kit's Portfolio</h1>
     </div>
   );
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css'
+
+export default function Footer(){
+    return(
+        <footer className='footer'>
+            <p>
+                Kit Williams
+            </p>
+        </footer>
+        )
+}

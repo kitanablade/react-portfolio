9216c80ae98e1a350255251f86cbf06080e76a52
kitanablade <kiteagle@gmail.com>
2022-09-04 13:07:35 -0700

    filled out portfolio page, added links to footer icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,29 +3,36 @@ import "./style.css";
 
 export default function Footer() {
   return (
-    <footer className="footer">
+    <footer className="navbar navbar-fixed-bottom">
       <div className="row justify-content-center">
         <div className="col-1">
-          <img
-            className="img-fluid"
-            src="images/Github/GitHub-Mark-32px.png"
-            alt="Github icon with embedded link"
-          ></img>{" "}
+          <a href="https://github.com/kitanablade">
+            <img
+              className="img-fluid"
+              src="images/Github/GitHub-Mark-64px.png"
+              alt="Github icon with embedded link"
+            ></img>{" "}
+          </a>
         </div>
         <div className="col-1">
           {" "}
+          <a href="www.linkedin.com/in/kit-williams-webdev">
           <img
             className="img-fluid"
-            src="images/LinkedIn/LI-In-Bug.png.png"
+            src="images/LinkedIn/LI-In-Bug.png"
             alt="LinkedIn icon with embedded link"
           ></img>
+          </a>
         </div>
         <div className="col-1">
+        <a href="https://twitter.com/kiteaglewx">
+
           <img
             className="img-fluid"
             src="images/Twitter/2021 Twitter logo - blue.png"
             alt="Twitter icon with embedded link"
           ></img>{" "}
+          </a>
         </div>
       </div>
     </footer>
--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,25 +1,122 @@
-import React,{} from 'react'
+import React from "react";
 
 export default function Portfolio() {
-    function activateLasers(e){
-        e.preventDefault();
-        console.log("LASERS ACTIVATED!!!");
-    }
-return(
+  // function activateLasers(e){
+  //     e.preventDefault();
+  //     console.log("LASERS ACTIVATED!!!");
+  // }
+  return (
     <div className="container">
-  <div className="row">
-    <div className="col-6"><img className="img-fluid" src="images/curarte.png" onClick={activateLasers}>
-    </img>
+      <div className="row">
+        <div className="col-6">
+          
+            <img
+              className="img-fluid"
+              src="images/curarte2.png"
+              alt="Screenshot of the Curarte app"
+            ></img>
+          
+          <div className="row">
+            <a href="https://kitanablade.github.io/curarte/">
+              Curarte Deployed Site
+            </a>
+          </div>
+          <div className="row">
+            <a href="https://github.com/kitanablade/curarte">
+              Curarte GitHub Repo
+            </a>
+          </div>
+          {/* https://github.com/kitanablade/scheduler */}
+          {/* https://kitanablade.github.io/scheduler/ */}
+        </div>
+        <div className="col-6">
+          <img
+            className="img-fluid"
+            src="images/scheduler.png"
+            alt="Screenshot of the Scheduler app"
+          ></img>
+          <div className="row h-25">
+            <a href="https://github.com/kitanablade/scheduler">
+              Work Day Scheduler Deployed Site
+            </a>
+          </div>
+          <div className="row">
+            <a href="https://kitanablade.github.io/scheduler/">
+              Work Day Scheduler GitHub Repo
+            </a>
+          </div>
+        </div>
+        <div className="w-100"></div>
+        <div className="col-6">
+          <img
+            className="img-fluid"
+            src="images/wx-dashboard.png"
+            alt="Screenshot of the Weather app"
+          ></img>
+          <div className="row">
+            <a href="https://kitanablade.github.io/weather-api/">
+            Weather Dashboard Deployed Site
+            </a>
+          </div>
+          <div className="row">
+            <a href="https://github.com/kitanablade/weather-api">
+              Weather Dashboard GitHub Repo
+            </a>
+          </div>
+        </div>
+        <div className="col-6">
+          <img
+            className="img-fluid"
+            src="images/farmers-market.png"
+            alt="Screenshot of the Farmer's Market app"
+          ></img>
+          <div className="row">
+            <a href="https://github.com/kitanablade/farmers-market-manager">
+              Farmer's Market Deployed Site
+            </a>
+          </div>
+          <div className="row">
+            <a href="https://github.com/kitanablade/farmers-market-manager">
+              Farmer's Market GitHub Repo
+            </a>
+          </div>
+        </div>
+        <div className="row"></div>
+        <div className="col-6">
+          <img
+            className="img-fluid"
+            src="images/github-regex-gist.png"
+            alt="Screenshot GitHub gist"
+          ></img>
+          <div className="row">
+            <a href="https://gist.github.com/kitanablade">
+              GitHub Gist about RegEx email verificaiton
+            </a>
+          </div>
+          {/* <div className="row">
+            <a href="https://github.com/kitanablade/curarte">
+              Curarte GitHub Repo
+            </a>
+          </div> */}
+        </div>
+        <div className="col-6">
+          <img
+            className="img-fluid"
+            src="images/quiz-game.png"
+            alt="Screenshot of the Quiz Game app"
+          ></img>
+          <div className="row">
+            <a href="https://kitanablade.github.io/quiz-timer/">
+              Quiz Game Deployed Site
+            </a>
+          </div>
+          <div className="row">
+            <a href="https://github.com/kitanablade/quiz-timer">
+              Quiz Game GitHub Repo
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-    <div className="col-6">Column</div>
-    <div className="w-100"></div>
-    <div className="col-6">Column</div>
-    <div className="col-6">Column</div>
-    <div className="row"></div>
-    <div className="col-6">Column</div>
-    <div className="col-6">Column</div>
-  </div>
-</div>
-)
-    
+  );
 }

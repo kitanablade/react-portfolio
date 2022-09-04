65007e16200c3cd0375b1585a6bafcaa97dba86a
kitanablade <kiteagle@gmail.com>
2022-09-04 14:05:40 -0700

    skills and resume page

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,11 +1,31 @@
-import React,{} from 'react'
+import React from "react";
 
 export default function Resume() {
-return(
+  return (
     <div className="Home">
-        <h1>Skills and Resume</h1>
-        <a href="images/williams-kit-resume.pdf" download>Kit's Resume Download (.pdf)</a>
+      <h1>Skills and Resume</h1>
+      <a href="files/williams-kit-resume.pdf" download>
+        Kit's Resume Download (.pdf)
+      </a>
+      <div className="row justify-content-center">
+        <div className="col-9">
+          <div className="row">
+            <div className ="col-4" id="skills-tab">Javascript</div>
+            <div className ="col-4" id="skills-tab">REACT</div>
+            <div className ="col-4" id="skills-tab">Node</div>
+          </div>
+          <div className="row">
+            <div className ="col-4" id="skills-tab">Express</div>
+            <div className ="col-4" id="skills-tab">MongoDB</div>
+            <div className ="col-4" id="skills-tab">SQL</div>
+          </div>
+          <div className="row">
+            <div className ="col-4" id="skills-tab">HTML</div>
+            <div className ="col-4" id="skills-tab">CSS</div>
+            <div className ="col-4" id="skills-tab">Typescript</div>
+          </div>
+        </div>
+      </div>
     </div>
-)
-    
+  );
 }

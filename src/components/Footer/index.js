939939d4f4304ad3044a7dd05c73e1065e3e5198
kitanablade <kiteagle@gmail.com>
2022-09-04 08:43:42 -0700

    added social media logos

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import './style.css'
+import React from "react";
+import "./style.css";
 
-export default function Footer(){
-    return(
-        <footer className='footer'>
-            <p>
-                Kit Williams
-            </p>
-        </footer>
-        )
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="row justify-content-center">
+        <div className="col-1">
+          <img
+            className="img-fluid"
+            src="images/Github/GitHub-Mark-32px.png"
+          ></img>{" "}
+        </div>
+        <div className="col-1">
+          {" "}
+          <img
+            className="img-fluid"
+            src="images/LinkedIn/LI-In-Bug.png.png"
+          ></img>
+        </div>
+        <div className="col-1">
+          <img
+            className="img-fluid"
+            src="images/Twitter/2021 Twitter logo - blue.png"
+          ></img>{" "}
+        </div>
+      </div>
+    </footer>
+  );
 }

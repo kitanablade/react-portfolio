12fccd775c3d2d29e8ad375c400da027b7ac4833
kitanablade <kiteagle@gmail.com>
2022-09-03 20:22:43 -0700

    stubbed out contact and portfolio pages

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,10 +1,34 @@
-import React,{} from 'react'
+import React from "react";
 
-export default function Contact() {
-return(
-    <div className="Home">
-        <h1>Contact</h1>
+export default function AboutMe() {
+  console.log("About Me Page");
+  return (
+    <div className= "row justify-content-center">
+    <div className="col-6">
+      <form>
+      <div className="form-group">
+    <label for="inputName">Name:</label>
+    <input type="text" className="form-control" id="inputName"></input>
+  </div>
+        <div className="form-group">
+          <label for="exampleFormControlInput1">Email address:</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com"
+          ></input>
+        </div>
+        <div className="form-group">
+          <label for="exampleFormControlTextarea1">Please enter your introduction, questions, or comments here!</label>
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+        </div>
+      </form>
     </div>
-)
-    
+    </div>
+  );
 }
--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,10 +1,25 @@
 import React,{} from 'react'
 
 export default function Portfolio() {
+    function activateLasers(e){
+        e.preventDefault();
+        console.log("LASERS ACTIVATED!!!");
+    }
 return(
-    <div className="Home">
-        <h1>This is the Portfolio Page.</h1>
+    <div className="container">
+  <div className="row">
+    <div className="col-6"><img className="img-fluid" src="images/curarte.png" onClick={activateLasers}>
+    </img>
     </div>
+    <div className="col-6">Column</div>
+    <div className="w-100"></div>
+    <div className="col-6">Column</div>
+    <div className="col-6">Column</div>
+    <div className="row"></div>
+    <div className="col-6">Column</div>
+    <div className="col-6">Column</div>
+  </div>
+</div>
 )
     
 }

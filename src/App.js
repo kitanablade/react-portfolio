3ce201d9195c7cccda2114fd61b1d2bfd894048a
kitanablade <kiteagle@gmail.com>
2022-09-06 11:07:30 -0700

    fixing hashroute issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import AboutMe from "./pages/AboutMe";
